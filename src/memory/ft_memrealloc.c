9260a0d3a4efcd3095cd10cefc632a0b3ce85432
Alexis Guérin <aguerin@student.42.fr>
2018-01-29 12:54:04 +0100

    Modification de commentaires

--- src/memory/ft_memrealloc.c
+++ src/memory/ft_memrealloc.c
@@ -7,7 +7,7 @@
 #include "libag.h"
 
 /**
-**	\brief	Réallocation de mémoire avec copie
+**	\brief	Changement d'une taille d'allocation
 **
 **	La fonction alloue une nouvelle zone de mémoire de taille _newsize_ et copie
 **	_old_ dans le nouveau pointeur. La mémoire de _old_ est libérée.
@@ -16,7 +16,14 @@
 **	indéterminé.
 **
 **	Voir : ft_memalloc() et ft_memcpy() de la [libft][].
-**	[Libft]: https://github.com/aguerin42/libft.git
+**	[libft]: https://github.com/aguerin42/libft.git
+**
+**	\param	old		- pointeur sur la zone mémoire à copier
+**	\param	oldsize	- taille de l'allocation de l'ancienne zone mémoire
+**	\param	newsize - taille de l'allocation pour la nouvelle zone mémoire
+**
+**	\return	pointeur sur la **nouvelle zone mémoire** allouée
+**			où l'ancienne a été copiée, ou **NULL** en cas d'erreur
 **/
 
 void	*ft_memrealloc(void *old, size_t oldsize, size_t newsize)
